e4af2dab1ebb7b12ab5a0214b0a3be29e65c0334
kevinmisati <kevinmosigisi2001@gmail.com>
2022-03-07 06:40:50 +0300

    linked lists

--- udemy_masterclass/singly_linked_list/app.js
+++ udemy_masterclass/singly_linked_list/app.js
@@ -1,22 +1,106 @@
-class Node {
+class Node{
     constructor(val){
         this.val = val
-        this.next = null
-    }
-}
-class SinglyLinkedList{
-    constructor(){
-        this.head = null
-        this.tail = tail
-        this.length = 0
-    }
-    push(val){
+        this.head=null
+        this.tail = null
         
     }
 }
 
-let first = new Node("Hi")
-first.next = new Node("there")
-first.next.next = new Node("how")
-first.next.next.next = new Node("are")
-first.next.next.next.next = new Node("you")
+class SinglyLinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.val = null
+        this.length = 0
+    }
+    push(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        }
+        else{
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+    traverse(){
+        let current = this.head
+        while(current){
+            console.log(current.val)
+            current = current.next
+        }
+    }
+    pop(){
+        if(!this.head){
+            return undefined
+        }
+        else{
+            let current = this.head
+            let newTail = current
+            while(current.next){
+                newTail = current
+                current = current.next
+            }
+            this.tail = newTail
+            this.tail.next = null
+            this.length--
+            return current
+        }
+    }
+
+    shift(){
+        if(!this.head){
+            return undefined
+        }
+        let currentHead = this.head
+        this.head = currentHead.next
+        this.length--
+        return currentHead
+    }
+    unshift(val){
+        let newHead = new Node(val)
+        if(!this.head){
+            this.head = newHead
+            this.tail = newHead
+        }
+        else{
+        let currentHead = this.head
+        this.head = newHead
+        this.head.next = currentHead
+        }
+        this.length++
+        return newHead
+}
+    get(index){
+        if (index < 0 || index > this.length){
+            return undefined
+        }
+        else{
+            let counter = 0
+            let current = this.head
+            while(counter != index){
+                counter++
+                current = current.next
+            }
+            return current
+        }
+    }
+}
+
+let list = new SinglyLinkedList()
+list.push("Hello")
+list.push("Welcome")
+list.push("Home")
+
+console.log(list.get(2))
+//console.log(list.unshift("Kevin"))
+//console.log(list.shift())
+//console.log(list.shift())
+//console.log(list)
+//console.log(list.pop())
+//console.log(list.pop())
+//
