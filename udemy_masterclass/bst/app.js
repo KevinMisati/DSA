d9dbbabe4145d1f54a3931dc8e69e6f6c16bba67
kevinmisati <kevinmosigisi2001@gmail.com>
2022-03-15 06:34:53 +0300

    finished max binary trees

--- udemy_masterclass/bst/app.js
+++ udemy_masterclass/bst/app.js
@@ -64,16 +64,81 @@ class BinarySearchTree{
 
         }
     }
+
+    bfs(){
+        let data = []
+        let queue = []
+        let node = this.root
+        queue.push(node)
+        while(queue.length){
+            node = queue.shift()
+            data.push(node)
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+        return data
+    }
+
+    preDFS(){
+        let data = []
+        let current = this.root
+        let traverse = (node) => {
+            data.push(node.value)
+            if(node.left) traverse(node.left)
+            if(node.right) traverse(node.right)
+        }
+        traverse(current)
+
+        return data
+    }
+    proDFS() {
+        let data = []
+        let current = this.root
+        let traverse = (node) => {
+            
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+            data.push(node.value)
+        }
+        traverse(current)
+
+        return data
+    }
+    inorderDFS() {
+        let data = []
+        let current = this.root
+        let traverse = (node) => {
+
+            if (node.left) traverse(node.left)
+            data.push(node.value)
+            if (node.right) traverse(node.right)
+        }
+        traverse(current)
+
+        return data
+    }
+    
 }
 
 let tree = new BinarySearchTree
 tree.insert(20)
 tree.insert(10)
 tree.insert(6)
-tree.insert(7)
+tree.insert(17)
 tree.insert(30)
+tree.insert(25)
 tree.insert(50)
-console.log(tree.find(20))
+//console.log(tree.find(20))
+//console.log(tree.bfs())
+//console.log(tree.preDFS())
+//console.log(tree.proDFS())
+console.log(tree.inorderDFS())
+ 
+//                                                20 
+//                                 10                             30
+//                    6                     17               25            50
+//                                    
+
 
 
 //console.log(tree)
