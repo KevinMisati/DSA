ab41fedb3879e8ca3d970d9f9fd47eb7326c0856
kevinmisati <kevinmosigisi2001@gmail.com>
2022-03-20 07:44:01 +0300

    finished graphs

--- udemy_masterclass/graphs/app.js
+++ udemy_masterclass/graphs/app.js
@@ -22,6 +22,67 @@ class Graph{
         }
         delete this.adjacencyList[v]
     }
+    depthFirstRecursive(start) {
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if (!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    return dfs(neighbor)
+                }
+            });
+        })(start);
+
+        return result;
+    }
+
+    depthFirstIterative(start){
+        const stack = [start]
+        const result = []
+        const visited = {}
+        let currentVertex;
+
+        visited[start] = true
+        while(stack.length){
+            currentVertex =  stack.pop()
+            result.push(currentVertex)
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true
+                    stack.push(neighbor)
+                }
+            })
+        }
+        return result
+    }
+
+    bfs(start) {
+        let queue = [start]
+        let visited = {}
+        let result = []
+        let currentVertex;
+
+        visited[start] = true
+
+        while(queue.length){
+            currentVertex = queue.shift()
+            result.push(currentVertex)
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true
+                    queue.push(neighbor)
+                }
+            })
+        }
+        return result
+    }
 }
 
 let g = new Graph()
@@ -38,5 +99,8 @@ g.addEdge("C","E")
 g.addEdge("D","E")
 g.addEdge("D","F")
 g.addEdge("E","F")
+//console.log(g.depthFirstRecursive("A"))
+//console.log(g.depthFirstIterative("A"))
+console.log(g.bfs("A"))
 //g.removeEdge("Tokyo","Aspen")
-console.log(g)
+//console.log(g)
